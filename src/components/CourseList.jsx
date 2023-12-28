44e47ef76f16d5731b7786a8d46dc7682b6fbda2
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-12-28 16:51:15 -0700

    course selection

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,35 +1,49 @@
 import React, { useState } from 'react';
 import SelectTerm from './SelectTerm';
-import { Box, Typography } from '@mui/material';
+import {ToggleButton, ToggleButtonGroup, Typography} from '@mui/material';
+
 
 const CourseList = ({ courses }) => {
     const terms = ['Fall', 'Spring', 'Winter'];
     const [selectedTerm, setSelectedTerm] = useState(terms);
+    const [selectedCourse, setSelectedCourse]=useState([]);
 
+    const handleChange = (_, newSelection) => {
+        setSelectedCourse(newSelection);
+    };
     const filteredCourses = Object.keys(courses)
-        .filter((courseKey) => selectedTerm.includes(courses[courseKey].term))
-        .map((courseKey) => (
-            <Box
-                key={courseKey}
+        .filter((key) => selectedTerm.includes(courses[key].term))
+        .map((key) => (
+            <ToggleButton
+                key={key}
+                value={key}
                 sx={{
-                    backgroundColor: 'white',
+                    backgroundColor: selectedCourse.includes(key) ? '#07ff15' : '#2196f3',
                     boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
                     borderRadius: '8px',
                     padding: '16px',
                     minWidth: '200px',
+                    margin: '8px',
+                    transition: 'background-color 0.1s',
+                    color: selectedCourse.includes(key) ? 'black' : 'white',
+                    '&:hover': {
+                        backgroundColor: selectedCourse.includes(key) ? '#45a049' : '#1565c0',
+                    },
                 }}
             >
-                <Typography variant="h6">{courses[courseKey].title}</Typography>
-                <Typography>{`Term: ${courses[courseKey].term}`}</Typography>
-                <Typography>{`Course Number: ${courses[courseKey].number}`}</Typography>
-                <Typography>{`Day/Time: ${courses[courseKey].meets}`}</Typography>
-            </Box>
+                <Typography variant="h6">{courses[key].title}</Typography>
+                <Typography>{`Term: ${courses[key].term}`}</Typography>
+                <Typography>{`Course Number: ${courses[key].number}`}</Typography>
+                <Typography>{`Day/Time: ${courses[key].meets}`}</Typography>
+            </ToggleButton>
         ));
 
     return (
         <div>
             <SelectTerm onSelectTerm={setSelectedTerm} selectedTerm={selectedTerm} terms={terms} />
-            <Box
+            <ToggleButtonGroup
+                value={selectedCourse}
+                onChange={handleChange}
                 sx={{
                     display: 'flex',
                     flexDirection: 'row',
@@ -38,7 +52,7 @@ const CourseList = ({ courses }) => {
                 }}
             >
                 {filteredCourses}
-            </Box>
+            </ToggleButtonGroup>
         </div>
     );
 };
